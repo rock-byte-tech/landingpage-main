d73e00effc40bf013ef803225d79ad07d36b5aed
Santiago A. <santiagoalvarezcarvajal@gmail.com>
2025-09-11 02:17:27 -0500

    Implementación de SEO y conexión con google Anlytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [sitemap()],
+  site: "https://rock-byte-tech.com.co",
+  trailingSlash: "never",
   vite: {
     plugins: [tailwindcss()]
   }
---
+++ src/global.d.ts
@@ -0,0 +1,4 @@
+interface Window {
+    AOS: any;
+    dataLayer?: any[];
+}
