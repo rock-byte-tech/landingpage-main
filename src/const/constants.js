441f0bdeeee59a23056c4adb2f4df3a9cf62aed8
Santiago A. <santiagoalvarezcarvajal@gmail.com>
2025-09-11 20:56:56 -0500

    Se cambia el mensaje del CTA inicial

--- src/const/constants.js
+++ src/const/constants.js
@@ -1,3 +1,3 @@
 export const URL_CALENDLY = "https://calendly.com/devops-rbt";
 export const PRIMARY_COLOR = "#00FFC6";
-export const CTA_LLAMADA = "¡Agendemos tu llamada gratis!";
+export const CTA_LLAMADA = "Coordina tu llamada inicial sin compromiso";
