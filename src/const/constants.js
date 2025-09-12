40db4480f7772c5cff1f1120b05dba43c45b8fc8
Santiago A. <santiagoalvarezcarvajal@gmail.com>
2025-09-11 21:00:08 -0500

    Segunda versión del CTA inicial

--- src/const/constants.js
+++ src/const/constants.js
@@ -1,3 +1,3 @@
 export const URL_CALENDLY = "https://calendly.com/devops-rbt";
 export const PRIMARY_COLOR = "#00FFC6";
-export const CTA_LLAMADA = "Coordina tu llamada inicial sin compromiso";
+export const CTA_LLAMADA = "Reserva tu cita sin costo";
