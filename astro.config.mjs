2b195595d0f04ba8024f0b403f16c07ff1bb7a47
Santiago A. <santiagoalvarezcarvajal@gmail.com>
2025-09-16 23:46:12 -0500

    Se agrega adaptador de verlcel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
   site: "https://rock-byte-tech.com.co",
   trailingSlash: "never",
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  output: 'static',
+  adapter: vercel()
 });
